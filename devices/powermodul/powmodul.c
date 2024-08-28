c1e018b8251bf8f54acb1b45068c9684992182f3
gpcm <gianpcm@gmail.com>
2024-08-27 19:57:21 -0700

    kernel module compiles

--- devices/powermodul/powmodul.c
+++ devices/powermodul/powmodul.c
@@ -1,15 +1,18 @@
-#include<linux/module.h>
-#include<linux/init.h>
-#include<linux/mod_devicetable.h>
-#include<linux/platform_device.h>
-#include<linux/property.h>
-#include<linux/of_device.h>
-#include<linux/gpio/consumer.h>
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/mod_devicetable.h>
+#include <linux/platform_device.h>
+#include <linux/property.h>
+#include <linux/of_device.h>
+#include <linux/gpio/consumer.h>
+#include <linux/interrupt.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("gpcm00");
+MODULE_DESCRIPTION("Power modulator device driver");
 
 static struct gpio_desc *zc = NULL;
 static struct gpio_desc *pw = NULL;
-
-static struct proc_dir_entry *pownmod = NULL;
 
 static int powm_probe(struct platform_device *pdev);
 static int powm_remov(struct platform_device *pdev);
@@ -25,7 +28,7 @@ static struct platform_driver powm_driver = {
 	.probe = powm_probe,
 	.remove = powm_remov,
 	.driver = {
-		.name = "power_modulator_driver"
+		.name = "power_modulator_driver",
 		.of_match_table = powm_ids,
 	},
 };
@@ -39,33 +42,33 @@ irqreturn_t placeholder(int irq_no, void *dev_id)
 static int powm_probe(struct platform_device *pdev) {
 	struct device *dev = &pdev->dev;
 	int gpio_irq;	
-	int ret = -1
+	int ret = -1;
 
-	printk("Initializing power modulator module\n");
+	pr_info("Initializing power modulator module\n");
 
 	zc = gpiod_get(dev, "zc", GPIOD_IN);
 	if(IS_ERR(zc)) {
-		pr_error("powm - Error: Could not set up ZC GPIO\n");
+		pr_err("powm - Error: Could not set up ZC GPIO\n");
 		ret = -IS_ERR(zc);
 		goto return_error;
 	} 
 	
 	pw = gpiod_get(dev, "pw", GPIOD_OUT_HIGH);
 	if(IS_ERR(pw)) {
-		pr_error("powm - Error: Could not set ip PW GPIO\n");
+		pr_err("powm - Error: Could not set ip PW GPIO\n");
 		ret = -IS_ERR(pw);
 		goto clear_zc;
 	}
 
 	gpio_irq = gpiod_to_irq(zc);
 	if(gpio_irq < 0) {
-		pr_error("powm - Error: Failed to get an IRQ number\n");
+		pr_err("powm - Error: Failed to get an IRQ number\n");
 		goto clear_pw;
 	}
 	
 	if(request_irq(gpio_irq, placeholder, 
 					IRQF_TRIGGER_RISING, "powm", NULL) < 0) {
-		pr_error("powm - Error: Failed to request IRQ\n");
+		pr_err("powm - Error: Failed to request IRQ\n");
 		goto clear_pw;
 	}
 
@@ -82,6 +85,7 @@ return_error:
 static int powm_remov(struct platform_device *pdev) {
 	printk("Removing power modulator module\n");
 	gpiod_put(zc);
+	return 0;
 }
 
-module_platform_driver(powm);
+module_platform_driver(powm_driver);
